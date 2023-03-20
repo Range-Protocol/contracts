edabd49b1911695a5a7c210afa0b1017daf4e5d6
0xbateman <0xbateman@rangeprotocol.com>
2023-03-20 18:12:14 +0800

    update deploy scripts

--- deploy/RangeProtocolFactory.deploy.ts
+++ deploy/RangeProtocolFactory.deploy.ts
@@ -1,15 +1,16 @@
 import { ethers } from "hardhat";
-
+import { LedgerSigner } from "@anders-t/ethers-ledger";
 async function main() {
+  const provider = ethers.getDefaultProvider("");
+  const ledger = await new LedgerSigner(provider, "");
   const UNI_V3_FACTORY = "0x1F98431c8aD98523631AE4a59f267346ea31F984";
-
-  const RangeProtocolFactory = await ethers.getContractFactory(
+  let RangeProtocolFactory = await ethers.getContractFactory(
     "RangeProtocolFactory"
   );
+  RangeProtocolFactory = await RangeProtocolFactory.connect(ledger);
   const factory = await RangeProtocolFactory.deploy(UNI_V3_FACTORY);
   console.log("Factory: ", factory.address);
 }
-
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
--- deploy/RangeProtocolVault.ts
+++ deploy/RangeProtocolVault.ts
@@ -1,26 +1,30 @@
 import { ethers } from "hardhat";
+import { LedgerSigner } from "@anders-t/ethers-ledger";
 import { getInitializeData } from "../test/common";
 
 async function main() {
-  const [manager] = await ethers.getSigners();
-  const RangeProtocolFactoryAddress = "0x773330693cb7d5D233348E25809770A32483A940";
-
+  const provider = ethers.getDefaultProvider(""); // To be updated.
+  const ledger = await new LedgerSigner(provider, ""); // To be updated.
+  const manager = ""; // To be updated.
+  const treasuryAddress = manager; // To be updated.
+  const managerAddress = manager; // To be updated.
+  const RangeProtocolFactoryAddress = "0x773330693cb7d5D233348E25809770A32483A940"; // To be updated.
   const token0 = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
   const token1 = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
-  const fee = 10000;
-  const treasuryAddress = manager.address;
-  const managerAddress = manager.address;
-  const managerFee = 500;
-  const lowerTick = -50000;
-  const upperTick = -40000;
-  const name = "Test Token";
-  const symbol = "TT";
+  const fee = 10000; // To be updated.
+  const managerFee = 500; // To be updated.
+  const name = "Test Token"; // To be updated.
+  const symbol = "TT"; // To be updated.
 
-  const factory = await ethers.getContractAt("RangeProtocolFactory", RangeProtocolFactoryAddress);
+  const factory = await ethers.getContractAt(
+    "RangeProtocolFactory",
+    RangeProtocolFactoryAddress
+  );
 
-  const RangeProtocolVault = await ethers.getContractFactory(
+  let RangeProtocolVault = await ethers.getContractFactory(
     "RangeProtocolVault"
   );
+  RangeProtocolVault = await RangeProtocolVault.connect(ledger);
   const vaultImpl = await RangeProtocolVault.deploy();
   const data = getInitializeData({
     treasuryAddress,
