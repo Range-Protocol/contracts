0bbe87049bdc7807140a78a594b48a9950c7ad30
0xbateman <0xbateman@rangeprotocol.com>
2023-03-21 11:35:30 +0800

    update deploy script

--- deploy/RangeProtocolVault.USDCWETH.deploy.ts
+++ deploy/RangeProtocolVault.USDCWETH.deploy.ts
@@ -6,26 +6,22 @@ async function main() {
   const provider = ethers.getDefaultProvider(""); // To be updated.
   const ledger = await new LedgerSigner(provider, ""); // To be updated.
   const manager = ""; // To be updated.
-  const treasuryAddress = manager; // To be updated.
-  const managerAddress = manager; // To be updated.
-  const RangeProtocolFactoryAddress = "0x773330693cb7d5D233348E25809770A32483A940"; // To be updated.
-  const token0 = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
-  const token1 = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
-  const fee = 10000; // To be updated.
-  const managerFee = 500; // To be updated.
-  const name = "Test Token"; // To be updated.
-  const symbol = "TT"; // To be updated.
+  const treasuryAddress = ""; // To be updated.
+  const managerAddress = ""; // To be updated.
+  const rangeProtocolFactoryAddress = "0x4bF9CDcCE12924B559928623a5d23598ca19367B"; // To be updated.
+  const vaultImplAddress = ""; // to be updated.
+  const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+  const fee = 500; // To be updated.
+  const managerFee = 0; // To be updated.
+  const name = ""; // To be updated.
+  const symbol = ""; // To be updated.
 
-  const factory = await ethers.getContractAt(
+  let factory = await ethers.getContractAt(
     "RangeProtocolFactory",
-    RangeProtocolFactoryAddress
+      rangeProtocolFactoryAddress
   );
-
-  let RangeProtocolVault = await ethers.getContractFactory(
-    "RangeProtocolVault"
-  );
-  RangeProtocolVault = await RangeProtocolVault.connect(ledger);
-  const vaultImpl = await RangeProtocolVault.deploy();
+  factory = await factory.connect(ledger);
   const data = getInitializeData({
     treasuryAddress,
     managerAddress,
@@ -35,10 +31,10 @@ async function main() {
   });
 
   const tx = await factory.createVault(
-    token0,
-    token1,
+    WETH,
+    USDC,
     fee,
-    vaultImpl.address,
+    vaultImplAddress,
     data
   );
   const txReceipt = await tx.wait();
