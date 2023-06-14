c13b963ab2ec18beac54b1609240c11a9ced1b09
0xbateman <0xbateman@rangeprotocol.com>
2023-06-14 17:31:51 +0800

    add userVault transfer in beforeTransfer hook  and manager balance underflow check

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,6 +23,13 @@ const config: HardhatUserConfig = {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
   networks: {
+    hardhat: {
+      forking: {
+        enabled: true,
+        url: "https://1rpc.io/bnb",
+      },
+      allowUnlimitedContractSize: true,
+    },
     mainnet: {
       accounts: PK ? [PK] : [],
       chainId: 1,
