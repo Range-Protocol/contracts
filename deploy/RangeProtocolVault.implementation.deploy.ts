fc78c10974c9ef657249e1fe4e45458d3aa987ef
0xbateman <0xbateman@rangeprotocol.com>
2023-07-18 15:32:28 +0800

    update deploy script to include native token support library

--- deploy/RangeProtocolVault.implementation.deploy.ts
+++ deploy/RangeProtocolVault.implementation.deploy.ts
@@ -1,21 +1,27 @@
 import { ethers } from "hardhat";
 import { LedgerSigner } from "@anders-t/ethers-ledger";
-import { getInitializeData } from "../test/common";
 
 async function main() {
-    const provider = ethers.getDefaultProvider(""); // To be updated.
-    const ledger = await new LedgerSigner(provider, ""); // To be updated.
-    let RangeProtocolVault = await ethers.getContractFactory(
-        "RangeProtocolVault"
-    );
-    RangeProtocolVault = await RangeProtocolVault.connect(ledger);
-    const vaultImpl = await RangeProtocolVault.deploy();
-    console.log(vaultImpl.address);
+  const provider = ethers.getDefaultProvider(""); // To be updated.
+  const ledger = await new LedgerSigner(provider, ""); // To be updated.
+
+  const NATIVE_TOKEN_SUPPORT = ""; // to be updated.
+  let RangeProtocolVault = await ethers.getContractFactory(
+    "RangeProtocolVault",
+    {
+      libraries: {
+        NativeTokenSupport: NATIVE_TOKEN_SUPPORT,
+      },
+    }
+  );
+  RangeProtocolVault = await RangeProtocolVault.connect(ledger);
+  const vaultImpl = await RangeProtocolVault.deploy();
+  console.log(vaultImpl.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
+  console.error(error);
+  process.exitCode = 1;
 });
