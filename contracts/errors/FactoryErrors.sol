e7fbe17af9e9222bc41bf5e27461231738cc9ad9
0xbateman <0xbateman@rangeprotocol.com>
2024-02-29 17:40:22 +0800

    first

---
+++ contracts/errors/FactoryErrors.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+library FactoryErrors {
+    error ZeroPoolAddress();
+    error NoVaultInitDataProvided();
+    error MismatchedVaultsAndImplsLength();
+    error VaultUpgradeFailed();
+    error ImplIsNotAContract();
+    error SameTokensAddresses();
+}
