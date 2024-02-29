223c12b0d3578c6eb658cd7e241fb13c40494b2c
0xbateman <0xbateman@rangeprotocol.com>
2024-02-29 17:42:59 +0800

    first

---
+++ contracts/algebra/core/contracts/interfaces/IAlgebraPool.sol
@@ -0,0 +1,238 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.8.4;
+
+/**
+ * @title The interface for a Algebra Pool
+ * @dev The pool interface is broken up into many smaller pieces.
+ * Credit to Uniswap Labs under GPL-2.0-or-later license:
+ * https://github.com/Uniswap/v3-core/tree/main/contracts/interfaces
+ */
+interface IAlgebraPool {
+    /**
+   * @notice Sets the initial price for the pool
+   * @dev Price is represented as a sqrt(amountToken1/amountToken0) Q64.96 value
+   * @param price the initial sqrt price of the pool as a Q64.96
+   */
+    function initialize(uint160 price) external;
+
+    /**
+     * @notice The contract that deployed the pool, which must adhere to the IAlgebraFactory interface
+     * @return The contract address
+     */
+    function factory() external view returns (address);
+
+    /**
+     * @notice The first of the two tokens of the pool, sorted by address
+     * @return The token contract address
+     */
+    function token0() external view returns (address);
+
+    /**
+     * @notice The second of the two tokens of the pool, sorted by address
+     * @return The token contract address
+     */
+    function token1() external view returns (address);
+
+    /**
+     * @notice The pool tick spacing
+     * @dev Ticks can only be used at multiples of this value
+     * e.g.: a tickSpacing of 60 means ticks can be initialized every 60th tick, i.e., ..., -120, -60, 0, 60, 120, ...
+     * This value is an int24 to avoid casting even though it is always positive.
+     * @return The tick spacing
+     */
+    function tickSpacing() external view returns (int24);
+
+    /**
+     * @notice The maximum amount of position liquidity that can use any tick in the range
+     * @dev This parameter is enforced per tick to prevent liquidity from overflowing a uint128 at any point, and
+     * also prevents out-of-range liquidity from being used to prevent adding in-range liquidity to a pool
+     * @return The max amount of liquidity per tick
+     */
+    function maxLiquidityPerTick() external view returns (uint128);
+
+    /**
+     * @notice Adds liquidity for the given recipient/bottomTick/topTick position
+     * @dev The caller of this method receives a callback in the form of IAlgebraMintCallback# AlgebraMintCallback
+     * in which they must pay any token0 or token1 owed for the liquidity. The amount of token0/token1 due depends
+     * on bottomTick, topTick, the amount of liquidity, and the current price.
+     * @param sender The address which will receive potential surplus of paid tokens
+     * @param recipient The address for which the liquidity will be created
+     * @param bottomTick The lower tick of the position in which to add liquidity
+     * @param topTick The upper tick of the position in which to add liquidity
+     * @param amount The desired amount of liquidity to mint
+     * @param data Any data that should be passed through to the callback
+     * @return amount0 The amount of token0 that was paid to mint the given amount of liquidity. Matches the value in the callback
+     * @return amount1 The amount of token1 that was paid to mint the given amount of liquidity. Matches the value in the callback
+     * @return liquidityActual The actual minted amount of liquidity
+     */
+    function mint(
+        address sender,
+        address recipient,
+        int24 bottomTick,
+        int24 topTick,
+        uint128 amount,
+        bytes calldata data
+    ) external returns (uint256 amount0, uint256 amount1, uint128 liquidityActual);
+
+    /**
+     * @notice Collects tokens owed to a position
+     * @dev Does not recompute fees earned, which must be done either via mint or burn of any amount of liquidity.
+     * Collect must be called by the position owner. To withdraw only token0 or only token1, amount0Requested or
+     * amount1Requested may be set to zero. To withdraw all tokens owed, caller may pass any value greater than the
+     * actual tokens owed, e.g. type(uint128).max. Tokens owed may be from accumulated swap fees or burned liquidity.
+     * @param recipient The address which should receive the fees collected
+     * @param bottomTick The lower tick of the position for which to collect fees
+     * @param topTick The upper tick of the position for which to collect fees
+     * @param amount0Requested How much token0 should be withdrawn from the fees owed
+     * @param amount1Requested How much token1 should be withdrawn from the fees owed
+     * @return amount0 The amount of fees collected in token0
+     * @return amount1 The amount of fees collected in token1
+     */
+    function collect(
+        address recipient,
+        int24 bottomTick,
+        int24 topTick,
+        uint128 amount0Requested,
+        uint128 amount1Requested
+    ) external returns (uint128 amount0, uint128 amount1);
+
+    /**
+     * @notice Burn liquidity from the sender and account tokens owed for the liquidity to the position
+     * @dev Can be used to trigger a recalculation of fees owed to a position by calling with an amount of 0
+     * @dev Fees must be collected separately via a call to #collect
+     * @param bottomTick The lower tick of the position for which to burn liquidity
+     * @param topTick The upper tick of the position for which to burn liquidity
+     * @param amount How much liquidity to burn
+     * @return amount0 The amount of token0 sent to the recipient
+     * @return amount1 The amount of token1 sent to the recipient
+     */
+    function burn(
+        int24 bottomTick,
+        int24 topTick,
+        uint128 amount
+    ) external returns (uint256 amount0, uint256 amount1);
+
+    /**
+     * @notice Swap token0 for token1, or token1 for token0
+     * @dev The caller of this method receives a callback in the form of IAlgebraSwapCallback# AlgebraSwapCallback
+     * @param recipient The address to receive the output of the swap
+     * @param zeroToOne The direction of the swap, true for token0 to token1, false for token1 to token0
+     * @param amountSpecified The amount of the swap, which implicitly configures the swap as exact input (positive), or exact output (negative)
+     * @param limitSqrtPrice The Q64.96 sqrt price limit. If zero for one, the price cannot be less than this
+     * value after the swap. If one for zero, the price cannot be greater than this value after the swap
+     * @param data Any data to be passed through to the callback. If using the Router it should contain
+     * SwapRouter#SwapCallbackData
+     * @return amount0 The delta of the balance of token0 of the pool, exact when negative, minimum when positive
+     * @return amount1 The delta of the balance of token1 of the pool, exact when negative, minimum when positive
+     */
+    function swap(
+        address recipient,
+        bool zeroToOne,
+        int256 amountSpecified,
+        uint160 limitSqrtPrice,
+        bytes calldata data
+    ) external returns (int256 amount0, int256 amount1);
+
+    /**
+     * @notice The globalState structure in the pool stores many values but requires only one slot
+     * and is exposed as a single method to save gas when accessed externally.
+     * @return price The current price of the pool as a sqrt(token1/token0) Q64.96 value;
+     * Returns tick The current tick of the pool, i.e. according to the last tick transition that was run;
+     * Returns This value may not always be equal to SqrtTickMath.getTickAtSqrtRatio(price) if the price is on a tick
+     * boundary;
+     * Returns fee The last pool fee value in hundredths of a bip, i.e. 1e-6;
+     * Returns timepointIndex The index of the last written timepoint;
+     * Returns communityFeeToken0 The community fee percentage of the swap fee in thousandths (1e-3) for token0;
+     * Returns communityFeeToken1 The community fee percentage of the swap fee in thousandths (1e-3) for token1;
+     * Returns unlocked Whether the pool is currently locked to reentrancy;
+     */
+    function globalState()
+        external
+        view
+        returns (
+            uint160 price,
+            int24 tick,
+            uint16 fee,
+            uint16 timepointIndex,
+            uint8 communityFeeToken0,
+            uint8 communityFeeToken1,
+            bool unlocked
+        );
+
+    /**
+     * @notice The fee growth as a Q128.128 fees of token0 collected per unit of liquidity for the entire life of the pool
+     * @dev This value can overflow the uint256
+     */
+    function totalFeeGrowth0Token() external view returns (uint256);
+
+    /**
+     * @notice The fee growth as a Q128.128 fees of token1 collected per unit of liquidity for the entire life of the pool
+     * @dev This value can overflow the uint256
+     */
+    function totalFeeGrowth1Token() external view returns (uint256);
+
+    /**
+     * @notice The currently in range liquidity available to the pool
+     * @dev This value has no relationship to the total liquidity across all ticks.
+     * Returned value cannot exceed type(uint128).max
+     */
+    function liquidity() external view returns (uint128);
+
+    /**
+     * @notice Look up information about a specific tick in the pool
+     * @dev This is a public structure, so the `return` natspec tags are omitted.
+     * @param tick The tick to look up
+     * @return liquidityTotal the total amount of position liquidity that uses the pool either as tick lower or
+     * tick upper;
+     * Returns liquidityDelta how much liquidity changes when the pool price crosses the tick;
+     * Returns outerFeeGrowth0Token the fee growth on the other side of the tick from the current tick in token0;
+     * Returns outerFeeGrowth1Token the fee growth on the other side of the tick from the current tick in token1;
+     * Returns outerTickCumulative the cumulative tick value on the other side of the tick from the current tick;
+     * Returns outerSecondsPerLiquidity the seconds spent per liquidity on the other side of the tick from the current tick;
+     * Returns outerSecondsSpent the seconds spent on the other side of the tick from the current tick;
+     * Returns initialized Set to true if the tick is initialized, i.e. liquidityTotal is greater than 0
+     * otherwise equal to false. Outside values can only be used if the tick is initialized.
+     * In addition, these values are only relative and must be used only in comparison to previous snapshots for
+     * a specific position.
+     */
+    function ticks(
+        int24 tick
+    )
+        external
+        view
+        returns (
+            uint128 liquidityTotal,
+            int128 liquidityDelta,
+            uint256 outerFeeGrowth0Token,
+            uint256 outerFeeGrowth1Token,
+            int56 outerTickCumulative,
+            uint160 outerSecondsPerLiquidity,
+            uint32 outerSecondsSpent,
+            bool initialized
+        );
+
+    /**
+     * @notice Returns the information about a position by the position's key
+     * @dev This is a public mapping of structures, so the `return` natspec tags are omitted.
+     * @param key The position's key is a hash of a preimage composed by the owner, bottomTick and topTick
+     * @return liquidityAmount The amount of liquidity in the position;
+     * Returns lastLiquidityAddTimestamp Timestamp of last adding of liquidity;
+     * Returns innerFeeGrowth0Token Fee growth of token0 inside the tick range as of the last mint/burn/poke;
+     * Returns innerFeeGrowth1Token Fee growth of token1 inside the tick range as of the last mint/burn/poke;
+     * Returns fees0 The computed amount of token0 owed to the position as of the last mint/burn/poke;
+     * Returns fees1 The computed amount of token1 owed to the position as of the last mint/burn/poke
+     */
+    function positions(
+        bytes32 key
+    )
+        external
+        view
+        returns (
+            uint128 liquidityAmount,
+            uint32 lastLiquidityAddTimestamp,
+            uint256 innerFeeGrowth0Token,
+            uint256 innerFeeGrowth1Token,
+            uint128 fees0,
+            uint128 fees1
+        );
+}
---
+++ contracts/interfaces/IRangeProtocolFactory.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+interface IRangeProtocolFactory {
+    event VaultCreated(address indexed uniPool, address indexed vault);
+    event VaultImplUpgraded(address indexed uniPool, address indexed vault);
+
+    function createVault(
+        address tokenA,
+        address tokenB,
+        address implementation,
+        bytes memory configData
+    ) external;
+
+    function upgradeVaults(address[] calldata _vaults, address[] calldata _impls) external;
+
+    function upgradeVault(address _vault, address _impl) external;
+}
